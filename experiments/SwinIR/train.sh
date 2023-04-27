0ce77f4b3b53279f72b04bf91307514002a7c161
zhiyuanyou <zhiyuanyou@foxmail.com>
2023-04-27 18:28:31 +0800

    style(exp): revise cfgs to give more hints

--- experiments/SwinIR/train.sh
+++ experiments/SwinIR/train.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=../../:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=2,3:$CUDA_VISIBLE_DEVICES
-python -m torch.distributed.launch --nproc_per_node=2 --master_port=5322 ../../basicsr/train.py -opt config.yml --launcher pytorch
+export CUDA_VISIBLE_DEVICES=$1
+python -m torch.distributed.launch --nproc_per_node=$2 --master_port=$3 ../../basicsr/train.py -opt config.yml --launcher pytorch
--- experiments/SwinIR/val.sh
+++ experiments/SwinIR/val.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=../../:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=$1:$CUDA_VISIBLE_DEVICES
+export CUDA_VISIBLE_DEVICES=$1
 python ../../basicsr/test.py -opt opt_val_div.yml --ckpt_iter $2
