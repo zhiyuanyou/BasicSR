b95f19c0423d338c05b14144fa2ed11ff6fe6154
zhiyuanyou <zhiyuanyou@foxmail.com>
2023-06-26 04:41:05 +0000

    feat(data): add shuffle for train & test

--- experiments_watermark/DF2K/check_data/check_data.py
+++ experiments_watermark/DF2K/check_data/check_data.py
@@ -1,9 +1,13 @@
-import glob
 import os
 
+import glob
+import random
+
+random.seed(131)
+
 if __name__ == "__main__":
-    lq_paths = sorted(glob.glob("/opt/data/share/xtkong/DF2K/mask_demask/*.png"))
-    gt_paths = sorted(glob.glob("/opt/data/share/xtkong/DF2K/GT_demask/*.png"))
+    lq_paths = sorted(glob.glob("/opt/data/share/xtkong/DF2K625/image/mask/*.png"))
+    gt_paths = sorted(glob.glob("/opt/data/share/xtkong/DF2K625/image/GT/*.png"))
     lq_names = [os.path.basename(lq_path) for lq_path in lq_paths]
     gt_names = [os.path.basename(gt_path) for gt_path in gt_paths]
     assert lq_names == gt_names
@@ -11,6 +15,7 @@ if __name__ == "__main__":
 
     p_train = 0.9
     num_train = int(len(lq_names) * p_train)
+    random.shuffle(lq_names)
     names_train = lq_names[:num_train]
     names_test = lq_names[num_train:]
     print(f"Num train: {len(names_train)}, Num test: {len(names_test)}")
